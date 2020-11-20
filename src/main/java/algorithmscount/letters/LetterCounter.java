70d818d208c488a71f80baffa3e35bd4e7553eb7
petersuha <peti.suha@gmail.com>
2020-11-20 15:05:52 +0100

    controlselection

---
+++ src/main/java/algorithmscount/letters/LetterCounter.java
@@ -0,0 +1,18 @@
+package algorithmscount.letters;
+
+public class LetterCounter {
+    public int countLetters(String s, char c) {
+
+        int counter = 0;
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == c) {
+                counter++;
+            }
+        }
+        return counter;
+
+    }
+
+
+}
