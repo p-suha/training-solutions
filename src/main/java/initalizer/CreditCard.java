d3aefe4c8cdb1ed34dcf4a28e483bc1c8540fe6f
petersuha <peti.suha@gmail.com>
2020-12-13 17:26:20 +0100

    initalizer

---
+++ src/main/java/initalizer/CreditCard.java
@@ -0,0 +1,55 @@
+package initalizer;
+
+import week07d01.MathAlgorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreditCard {
+
+    private static final List<Rate> ACTUAL_RATES = new ArrayList<>();
+
+    private long balance;
+
+    public CreditCard(long balance, Currency currency, List<Rate> upload) {
+        ACTUAL_RATES.addAll(upload);
+        this.balance = Math.round(balance * getConversionRate(currency));
+    }
+
+    public CreditCard(long balance) {
+        this.balance = balance;
+    }
+
+
+    public static List<Rate> getActualRates() {
+        return ACTUAL_RATES;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public boolean payment(long amount, Currency currency) {
+        long value = Math.round(amount * getConversionRate(currency));
+        if (value <= balance) {
+            balance -= value;
+            return true;
+        }
+        return false;
+    }
+
+
+    public boolean payment(long amount) {
+        return payment(amount, Currency.HUF);
+    }
+
+    private double getConversionRate(Currency currency) {
+
+        for (Rate r : getActualRates()) {
+            if (r.getCurrency() == currency) {
+                return r.getConversionFactor();
+            }
+        }
+        return 1.0;
+    }
+}
---
+++ src/main/java/initalizer/Currency.java
@@ -0,0 +1,12 @@
+package initalizer;
+
+public enum Currency {
+
+    HUF,
+    EUR,
+    SFR,
+    GBP,
+    USD
+
+
+}
