f719eedc6bee217692c2e6f927ca467a82c9e716
petersuha <peti.suha@gmail.com>
2020-12-30 13:47:48 +0100

    objectclass

---
+++ src/main/java/objectclass/Book.java
@@ -0,0 +1,38 @@
+package objectclass;
+
+public class Book {
+
+    private String author;
+    private String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+
+        Book book = (Book) o;
+
+        if (author != null ? !author.equals(book.author) : book.author != null) return false;
+        return title != null ? title.equals(book.title) : book.title == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = author != null ? author.hashCode() : 0;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        return result;
+    }
+}
