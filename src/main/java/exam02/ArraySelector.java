7fe391e1608c8de66d8fc0619622d1ebeeaaa27f
petersuha <peti.suha@gmail.com>
2021-01-06 17:40:38 +0100

    exam02arrayselector

--- src/main/java/exam02/ArraySelector.java
+++ src/main/java/exam02/ArraySelector.java
@@ -1,9 +1,38 @@
 package exam02;
 
+//Tömbök
+//Írj egy olyan metódust az ArraySelector osztályban, mely egy kapott tömbből minden második elemet kiválogat,
+// és egy szövegként összerak, szögletes zárójelek között!
+//
+//A páros megállapítása index szerint működik. Azaz az első, nulla indexű elem páros,
+// a második, azaz egyes indexű elem páratlan, stb.
+//
+//Ha nulla elemű a tömb, akkor üres stringet adjon vissza!
+
 public class ArraySelector {
 
-    public String selectEvens(int[] array) {
 
-        return null;
+    public String selectEvens(int[] array) {
+        StringBuilder strb = new StringBuilder();
+
+        strb.append("[");
+
+        if (array.length == 0) {
+
+            return "";
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                if (i % 2 == 0) {
+                    strb.append(array[i] + ", ");
+                }
+
+            }
+        }
+        String resultString = strb.substring(0, strb.length() - 2) + "]";
+
+        return resultString;
     }
+
+
 }
+
