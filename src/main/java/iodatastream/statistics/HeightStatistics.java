d54eb739151061b5f4b24915010529548a65726c
petersuha <peti.suha@gmail.com>
2021-02-02 21:00:52 +0100

    iodatastream

---
+++ src/main/java/iodatastream/statistics/HeightStatistics.java
@@ -0,0 +1,23 @@
+package iodatastream.statistics;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class HeightStatistics {
+
+    public void saveHeights(List<Integer> heights, Path path) {
+        try (DataOutputStream os = new DataOutputStream(new BufferedOutputStream(Files.newOutputStream(path)))) {
+
+            os.writeInt(heights.size());
+            for (Integer heigt : heights) {
+                os.writeInt(heigt);
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Cannot write file", e);
+        }
+    }
+}
