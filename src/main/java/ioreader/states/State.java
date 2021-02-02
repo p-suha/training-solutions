62f79ab2944b34806f92f81da550c911393f1708
petersuha <peti.suha@gmail.com>
2021-02-02 13:30:20 +0100

    ioreader

---
+++ src/main/java/ioreader/states/State.java
@@ -0,0 +1,35 @@
+package ioreader.states;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class State {
+
+    private String stateName;
+    private String capital;
+
+    public State(String stateName, String capital) {
+        this.stateName = stateName;
+        this.capital = capital;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+}
