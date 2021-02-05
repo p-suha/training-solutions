64be21517b011a2628c07f1c5a60b4c92c58d929
petersuha <peti.suha@gmail.com>
2021-02-05 13:26:00 +0100

    collectionsqueue

---
+++ src/main/java/collectionsqueue/job/NoJobException.java
@@ -0,0 +1,15 @@
+package collectionsqueue.job;
+
+public class NoJobException extends Exception {
+
+    private final String message;
+
+    public NoJobException(String str) {
+        this.message = str;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
