515d21b069cd179b1b0b753b494e3f2819ad04bf
petersuha <peti.suha@gmail.com>
2021-02-15 18:33:44 +0100

    exam03

--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -1,5 +1,34 @@
 package exam03;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Histogram {
 
+    public String createHistogram(String file) {
+        Path path = Path.of(file);
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+
+                int numFromText = Integer.parseInt(line);
+                for (int i = 0; i < numFromText; i++) {
+                    sb.append("*");
+                }
+                sb.append("\n");
+
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Can not read file", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Histogram h = new Histogram();
+        System.out.println(h.createHistogram("histogram.txt"));
+    }
 }
--- src/test/java/exam03/HistogramTest.java
+++ src/test/java/exam03/HistogramTest.java
@@ -12,8 +12,8 @@
 //
 //    @Test
 //    void createHistogram() throws IOException {
-//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(HistogramTest.class.getResourceAsStream("histogram.txt")))) {
-//            String result = new Histogram().createHistogram(reader);
+//   //     try (BufferedReader reader = new BufferedReader(new InputStreamReader(HistogramTest.class.getResourceAsStream("src/test/histogram.txt")))) {
+//        //    String result = new Histogram().createHistogram(reader);
 ////            System.out.println(result);
 //            assertEquals("***\n" +
 //                    "****\n" +
